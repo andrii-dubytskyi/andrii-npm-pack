52275017dd84a7b15b88da299694a851ea876553
Andrii Dubytskyi <andrii.dubytskyi@gmail.com>
2021-05-15 15:13:56 -0500

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import yargs from 'yargs';
+
+
+/**
+ * Arguments Parser
+ */
+const argv = yargs
+
+  // Framework Initialize
+  .command('configure', 'Initialize framework', 
+    (yargs) => {}, 
+    (argv) => {
+      console.log('>>> Configure');
+    }
+  )
+  
+  // Framework Setup
+  .command('setup', 'Scaffolds framework infrastructure based on the configuration', 
+    (yargs) => {}, 
+    (argv) => {
+      console.log('>>> Setup');
+    }
+  )
+
+  .option('name', {
+    alias: 'n',
+    type: 'string'
+  })
+  .option('verbose', {
+    alias: 'v',
+    type: 'string',
+  })
+  .option('overwrite', {
+    alias: 'o',
+    type: 'boolean',
+    default: true
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
